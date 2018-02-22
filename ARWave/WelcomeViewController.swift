e36f7d6b595a31efa1cf8edb0c4481a44e949d62
Leon Lee <leon.lee@berkeley.edu>
2018-02-22 15:56:21 -0800

    draft welcome part sliding show

--- ARWave/WelcomeViewController.swift
+++ ARWave/WelcomeViewController.swift
@@ -7,29 +7,51 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class WelcomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var slideshow: ImageSlideshow!
+    
+    let localSource = [ImageSource(imageString: "welcome_1")!, ImageSource(imageString: "welcome_2")!, ImageSource(imageString: "welcome_3")!]
+    let afNetworkingSource = [AFURLSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, AFURLSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, AFURLSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    let alamofireSource = [AlamofireSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    let sdWebImageSource = [SDWebImageSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, SDWebImageSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, SDWebImageSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    let kingfisherSource = [KingfisherSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, KingfisherSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, KingfisherSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        slideshow.backgroundColor = UIColor(red: (235/255.0), green: (224/255.0), blue: (196/255.0), alpha: 1.0)
+        slideshow.slideshowInterval = 0
+        slideshow.circular = false
+        slideshow.pageControlPosition = PageControlPosition.insideScrollView
+        slideshow.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
+        slideshow.pageControl.pageIndicatorTintColor = UIColor.black
+        slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
+        
+        // optional way to show activity indicator during image load (skipping the line will show no activity indicator)
+        slideshow.activityIndicator = DefaultActivityIndicator()
+        slideshow.currentPageChanged = { page in
+            print("current page:", page)
+            if (page == 2) {
+//                print("I am in page 3")
+                let swipeRecognizer = UISwipeGestureRecognizer(target: self, action:)
+                
+                
+            }
+        }
+        
+        // can be used with other sample sources as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
+        slideshow.setImageInputs(localSource)
+        
+//        let recognizer = UITapGestureRecognizer(target: self, action: #selector(WelcomeViewController.didTap))
+//        slideshow.addGestureRecognizer(recognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @objc func didTap() {
+        let fullScreenController = slideshow.presentFullScreenController(from: self)
+        // set the activity indicator for full screen controller (skipping the line will show no activity indicator)
+        fullScreenController.slideshow.activityIndicator = DefaultActivityIndicator(style: .white, color: nil)
     }
-    */
-
 }
--- Podfile
+++ Podfile
@@ -1,5 +1,9 @@
 platform :ios,'8.0'
 target 'ARWave' do
 pod 'ImageSlideshow', '~> 1.5.1'
+pod "ImageSlideshow/AFURL"
+pod "ImageSlideshow/Alamofire"
+pod "ImageSlideshow/SDWebImage"
+pod "ImageSlideshow/Kingfisher"
 use_frameworks!
 end
