9d6a9e9fa8f93f5148058995e40f7140b2850546
Leon Lee <leon.lee@berkeley.edu>
2018-05-05 22:57:54 -0700

    draft skeleton

---
+++ ARWave/BeforeWelcomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BeforeWelcomeViewController.swift
+//  ARWave
+//
+//  Created by Leon Lee on 5/5/18.
+//  Copyright © 2018 Leon Lee. All rights reserved.
+//
+
+import UIKit
+
+class BeforeWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindSegueToBeforeWelcome(_ sender: UIStoryboardSegue) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
